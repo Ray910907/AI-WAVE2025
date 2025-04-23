468a789011772b1f4125e4cd5bb0c3cbc13d5bc1
Allen-Su-me <109009788+Allen-Su-me@users.noreply.github.com>
2025-04-23 22:19:05 +0800

    Add lambda_function.py

---
+++ AWS/lambda_function.py
@@ -0,0 +1,29 @@
+import os
+import io
+import boto3
+import json
+import csv
+
+# Grab environment variables
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime= boto3.client('runtime.sagemaker')
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    
+    # Expecting request as a python dictionary with {"data" : sample} format
+    payload = event['data']
+    print(payload)
+    
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       Body=payload,
+                                       ContentType="text/csv"
+                                       )
+    print(f'res:{response}')
+    result = response["Body"].read().decode("utf-8")
+    print(result)
+    pred = float(result)
+    predicted_label = (f'Prediction score : {float(result)}, Prediction : warn' if pred >= 0.5
+                      else f'Prediction score : {float(result)}, Prediction : not-warn'
+                      )
+    return predicted_label
